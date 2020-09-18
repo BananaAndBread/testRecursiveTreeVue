13d6e19b889ec80e3a8360e8c18b8a6032555e6b
BananaAndBread <vaskovskaya.dasha2@yandex.ru>
2020-09-18 15:51:54 +0300

    first commit

---
+++ src/components/TreeNode.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="tree-menu">
+    <div>{{ text }}</div>
+    <TreeNode
+        v-bind:key="index"
+        v-for="(node, index) in childs"
+        :childs="node.childs"
+        :label="node.text"
+    >
+    </TreeNode>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+    },
+    childs: {
+      type: Array
+    }
+  },
+  name: 'TreeNode'
+}
+</script>
+
+<style scoped>
+
+
+</style>
